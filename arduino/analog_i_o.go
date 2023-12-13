fa639f1e6268d1f45d8161c0d5844bdcdd4dfe55
usuyuki <63891531+usuyuki@users.noreply.github.com>
2023-12-14 00:00:16 +0900

    feat analog

--- arduino/analog_i_o.go
+++ arduino/analog_i_o.go
@@ -1,1 +1,14 @@
+// Arduino compatible functions
 package arduino
+
+// Equivalent to Arduino's analogRead https://www.arduino.cc/reference/en/language/functions/analog-io/analogread/
+//
+//go:wasm-module arduino
+//go:export analogRead
+func AnalogRead(pin uint8) uint16
+
+// Equivalent to Arduino's analogWrite https://www.arduino.cc/reference/en/language/functions/analog-io/analogwrite/
+//
+//go:wasm-module arduino
+//go:export analogWrite
+func AnalogWrite(pin uint8, value int)
