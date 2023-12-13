a331b231d5c0d6fc87088794fae881d4333db579
usuyuki <63891531+usuyuki@users.noreply.github.com>
2023-12-14 00:00:10 +0900

    feat digital

--- arduino/digital_i_o.go
+++ arduino/digital_i_o.go
@@ -1,1 +1,20 @@
+// Arduino compatible functions
 package arduino
+
+// Equivalent to Arduino's digitalRead https://www.arduino.cc/reference/en/language/functions/digital-io/digitalread/
+//
+//go:wasm-module arduino
+//go:export digitalRead
+func DigitalRead(pin uint8) int
+
+// Equivalent to Arduino's digitalWrite https://www.arduino.cc/reference/en/language/functions/digital-io/digitalwrite/
+//
+//go:wasm-module arduino
+//go:export digitalWrite
+func DigitalWrite(pin uint8, value int)
+
+// Equivalent to Arduino's pinMode https://www.arduino.cc/reference/en/language/functions/digital-io/pinmode/
+//
+//go:wasm-module arduino
+//go:export pinMode
+func PinMode(pin uint8, mode uint8)
