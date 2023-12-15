ab0e887bad76fe2ac5f025fcf720064cdb75f85e
usuyuki <63891531+usuyuki@users.noreply.github.com>
2023-12-15 12:29:59 +0900

    wip tinygo-func

---
+++ _examples/not_support/tinygo_func_test/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/project-mahiwa/mahiwa-frontend-go/arduino"
+	"github.com/project-mahiwa/mahiwa-frontend-go/serial"
+)
+
+// goroutineをしたとき、wasmでどう表現されているのかを見る。
+func main() {
+	serial.Println("TinyGo Func Test")
+	arduino.Delay(1000) // mainルーチンをgoroutineより先に終了させないためのdelay
+	// 普通のgoでは
+	// main routine
+	// goroutine
+	// の順で表示されるが、mahiwaで動かすと
+	// main routine
+	// Error: [trap] stack overflow ()
+	// となり、実行不可
+}
