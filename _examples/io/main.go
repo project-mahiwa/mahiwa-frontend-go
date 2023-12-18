6acc11e896b420a39b927095cff7a89d6a9fe5cd
usuyuki <63891531+usuyuki@users.noreply.github.com>
2023-12-18 16:28:32 +0900

    fix printする

--- _examples/io/main.go
+++ _examples/io/main.go
@@ -11,8 +11,10 @@ func main() {
 	arduino.PinMode(3, arduino.OUTPUT)
 	for {
 		arduino.DigitalWrite(3, arduino.HIGH)
+		serial.Println("change")
 		arduino.Delay(1000)
 		arduino.DigitalWrite(3, arduino.LOW)
+		serial.Println("change")
 		arduino.Delay(1000)
 	}
 }
