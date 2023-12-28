9c01df1069bb4dc8bd3dd6ceeb173e07e7fb2911
usuyuki <63891531+usuyuki@users.noreply.github.com>
2023-12-28 16:06:16 +0900

    fix 素数判定の実装揃える

--- _benchmarks/prime/main.go
+++ _benchmarks/prime/main.go
@@ -6,11 +6,12 @@ import (
 	"github.com/project-mahiwa/mahiwa-frontend-go/serial"
 )
 
-func isPrime(num int) bool {
+func isPrime(num int32) bool {
 	if num <= 1 {
 		return false
 	}
-	for i := 2; float64(i) <= math.Sqrt(float64(num)); i++ {
+	numMax := int32(math.Sqrt(float64(num)))
+	for i := int32(2); i <= numMax; i++ {
 		if num%i == 0 {
 			return false
 		}
